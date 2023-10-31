746b3b490def8d78c3f81da0a1524e110fde7ac6
shobhan28 <shobhan28m@gmail.com>
2023-10-31 18:18:34 +0530

    created dto class

---
+++ src/main/java/com/otpverification/payload/PhoneDto.java
@@ -0,0 +1,13 @@
+package com.otpverification.payload;
+
+public class PhoneDto {
+    private String phoneNumber;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
